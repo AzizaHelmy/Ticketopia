f96966fe3b0935e408ab89328ae99fffb4d01844
Aziza <aziza.helmy35@gmail.com>
2023-07-08 17:11:14 +0300

    remove unnecessary code

--- app/src/main/java/com/example/ticketopia/ui/MainActivity.kt
+++ app/src/main/java/com/example/ticketopia/ui/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.ticketopia.ui
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
